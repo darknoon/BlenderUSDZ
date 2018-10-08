187872214279581ed3b15aa649ec207641ca48a0
Andrew Pouliot <andrew@darknoon.com>
2018-10-08 12:41:27 -0400

    Script that symlinks this repo in your user scripts for ease of editing

---
+++ symlink.py
@@ -0,0 +1,13 @@
+import os
+
+
+def symlink_relative(src, dst):
+    src = os.path.expanduser(src)
+    src = os.path.abspath(src)
+    dst = os.path.expanduser(dst)
+    dst = os.path.abspath(dst)
+    os.symlink(src, dst, True)
+
+
+symlink_relative('./io_export_usd',
+                 '~/Library/Application Support/Blender/2.79/scripts/addons/io_export_usd')
