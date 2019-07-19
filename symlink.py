5065d337a950052db485d4deab175168b3a0ef34
Andrew Pouliot <andrew@darknoon.com>
2019-07-19 11:01:51 -0400

    symlink 2.8 instead of 2.79

--- symlink.py
+++ symlink.py
@@ -6,8 +6,14 @@ def symlink_relative(src, dst):
     src = os.path.abspath(src)
     dst = os.path.expanduser(dst)
     dst = os.path.abspath(dst)
+    parent = os.path.dirname(dst)
+    if not os.path.exists(parent):
+        os.makedirs(parent)
     os.symlink(src, dst)
 
 
+# symlink_relative('./io_export_usd',
+#                  '~/Library/Application Support/Blender/2.79/scripts/addons/io_export_usd')
+
 symlink_relative('./io_export_usd',
-                 '~/Library/Application Support/Blender/2.79/scripts/addons/io_export_usd')
+                 '~/Library/Application Support/Blender/2.80/scripts/addons/io_export_usd')
